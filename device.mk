1f147b348a9dae6fd20f9001dd5b118a98bbd1c1
raghavt20 <raghavtandon2000@gmail.com>
2021-10-06 11:49:27 +0530

    violet: Don't build XiaomiParts

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Device-specific settings
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     XiaomiParts
 
 # Display
